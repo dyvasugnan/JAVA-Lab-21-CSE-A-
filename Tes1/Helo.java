38302d900e1f5f7bc337c572e1a4b88ca91e1209
Sudha4884 <85675476+Sudha4884@users.noreply.github.com>
2023-06-16 15:37:03 +0530

    Add files via upload

---
+++ Tes1/Helo.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+public class A{
+public static void main(String args[]){
+System.out.print("A Class");
+}
+}
+public class B{
+public static void main(String args[]){
+System.out.print("B Class");
+}
+}
+public class C{
+public static void main(String args[]){
+System.out.print("C Class");
+}
+}
+public class D{
+}
