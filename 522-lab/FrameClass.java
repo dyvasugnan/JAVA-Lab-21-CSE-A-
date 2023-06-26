f695a3f5a313703c1a4c22648c4b245b6ac9cd4e
21wh1a0522 <121939368+21wh1a0522@users.noreply.github.com>
2023-06-26 12:16:46 +0530

    Add files via upload

---
+++ 522-lab/FrameClass.java
@@ -0,0 +1,41 @@
+package javalab;
+import java.lang.*;
+import java.util.*;
+import java.awt.*; // abstract window toolkit
+import java.awt.event.*;
+public class FrameClass extends Frame{
+	public void paint(Graphics g) {
+		this.setBackground(Color.black);
+		Font f = new Font("Arial",Font.BOLD,30);
+		g.setFont(f);
+		g.drawString("EMOJI FACE",200,100);
+		//g.drawRect(100,100,500,50);
+		g.drawOval(100,100,200,200);
+		g.setColor(Color.red);
+		g.fillOval(100,100,200,200);
+		g.drawOval(180, 180, 20, 20);
+		g.setColor(Color.yellow);
+		g.fillOval(150, 150, 20, 20);
+		g.fillOval(220, 150, 20, 20);
+		g.setColor(Color.yellow);
+		g.drawLine(200, 220, 200, 200);
+		g.setColor(Color.yellow);
+		g.drawArc(160, 250, 100, 100, 40, 100);
+	}
+
+	public static void main(String[] args){
+		FrameClass f=new FrameClass();
+		//f.setTitle("");
+		
+		f.setSize(400,400);
+		f.setVisible(true);
+		f.addWindowListener(new Myclass());
+	}
+}
+
+
+class Myclass extends WindowAdapter{
+	public void windowClosing(WindowEvent we) {
+		System.exit(0);
+	}
+}
