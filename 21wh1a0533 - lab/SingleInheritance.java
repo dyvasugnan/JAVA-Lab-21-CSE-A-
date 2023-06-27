b7bd804ebe6e61058af7e3bba86eebc2224882e5
21wh1a0533 <137030356+21wh1a0533@users.noreply.github.com>
2023-06-28 00:29:55 +0530

    Add files via upload

---
+++ 21wh1a0533 - lab/SingleInheritance.java
@@ -0,0 +1,31 @@
+package javaclass;
+
+//Parent class
+class Base{
+ void show(int num1) {
+     System.out.println("num1: " + num1);
+ }
+}
+
+//Child class inheriting from the parent class
+class Derived extends Base {
+ int num2;
+ void display(int num2){
+     System.out.println("num2: " + num2);
+ }
+}
+
+//Main class
+class SingleInheritance{
+ public static void main(String args[]) {
+     // Create an instance of the child class
+     Derived d = new Derived();
+
+     // Call methods from the parent class
+     d.show(10);
+
+     // Call methods specific to the child class
+     d.display(20);
+ }
+}
+
