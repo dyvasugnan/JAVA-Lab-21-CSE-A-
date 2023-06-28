0f0748a4e8ceeedc770380a10e863a84852fb651
21wh1a0535 <137029727+21wh1a0535@users.noreply.github.com>
2023-06-28 18:12:29 +0530

    Add files via upload

---
+++ 21WH1A0535-class/EvenOrOdd.java
@@ -0,0 +1,12 @@
+//Even or Odd
+import java.util.*;
+class EvenOrOdd{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        if(n%2==0)
+        System.out.println("Even Number");
+        else
+        System.out.println("Odd Number");
+    }
+}
