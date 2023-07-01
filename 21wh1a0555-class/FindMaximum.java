7f7978a1780d84f7a04a8a9f911e9afece4b6f72
Ushashwini128 <136068686+Ushashwini128@users.noreply.github.com>
2023-07-01 20:52:50 +0530

    Add files via upload

---
+++ 21wh1a0555-class/FindMaximum.java
@@ -0,0 +1,24 @@
+package hello;
+import java.lang.*;
+import java.util.*;
+public class FindMaximum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        int max = a[0];
+        for(int i=0;i<n;i++){
+            if(a[i]>=max){
+                max=a[i];
+            }
+        }
+        System.out.print(+max);
+
+	}
+
+}
