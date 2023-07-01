9494f4661b9eb3060ac52e38bbe1c10a681a411e
Ushashwini128 <136068686+Ushashwini128@users.noreply.github.com>
2023-07-01 20:27:27 +0530

    Add files via upload

--- 21wh1a0555-class/FindSeason.java
+++ 21wh1a0555-class/FindSeason.java
@@ -1,5 +1,23 @@
 package hello;
+import java.lang.*;
+import java.util.*;
 
 public class FindSeason {
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        String mnth = sc.next();
+        int day = sc.nextInt();
+        if((mnth.equals("march") && day>=20) || (mnth.equals("april")) ||(mnth.equals("may")) ||(mnth.equals("june") && day<21)){
+            System.out.print("spring");
+        }
+        else if((mnth.equals("june") && day>=21) || (mnth.equals("july")) ||(mnth.equals("august")) || (mnth.equals("september") && day<22)){
+            System.out.print("summer");
+        }
+        else if((mnth.equals("september") && day>=22) || (mnth.equals("october")) || (mnth.equals("november"))||(mnth.equals("december") && day<21)){
+            System.out.print("fall");
+        }
+        else{
+            System.out.print("winter");
+        }
+	}
 }
