d0ed146f849da0dd83e981ec4c564fe1bea9df42
Ushashwini128 <136068686+Ushashwini128@users.noreply.github.com>
2023-07-01 20:34:43 +0530

    Add files via upload

---
+++ 21wh1a0555-class/EvenNumberSum.java
@@ -0,0 +1,19 @@
+package hello;
+import java.lang.*;
+import java.util.*;
+ class EvenNumberSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum=0,even=2;
+        for(int i=0;i<n;i++){
+            sum = sum+even;
+            even = even+2;
+        }
+        System.out.print(+sum);
+
+	}
+
+}
