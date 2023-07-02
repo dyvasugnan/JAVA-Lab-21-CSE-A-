2da60e633e3367421e27cf5855cffcdb17a45f85
21wh1a0523 <137030067+21wh1a0523@users.noreply.github.com>
2023-07-02 21:57:53 +0530

    Update SingleLevel.java

--- 523/SingleLvel.java
+++ 523/SingleLvel.java
@@ -1,5 +1,23 @@
 package javaclss;
-
-public class SingleLvel {
-
+import java.lang.*;
+public class SingleLevel {
+	 public static void main(String args[]){
+	        Derived obj = new Derived();
+	        obj.setx(10);
+	        obj.display();
+	    }
 }
+	class BaseClass{
+	    int x;
+	    void setx(int i){
+	        x=i;
+	        System.out.println(x);
+	        System.out.println("Base class");
+	    }
+}
+	class Derived extends BaseClass{
+	    void display(){
+	        System.out.println("Derived class");
+	    }
+
+}	
