02d183f35ff816505964a5a95e8d26e0fd50b723
21wh1a0536 <137029956+21wh1a0536@users.noreply.github.com>
2023-07-02 20:43:22 +0530

    Create Fibnocci.java

---
+++ 21WH1A0536 - Class/Fibnocci.java
@@ -0,0 +1,24 @@
+//fibonacci
+import java.lang.*;
+import java.util.Scanner;
+
+public class Fibnocci{
+    public static int fibonacci (int n) {
+         if (n <= 0) 
+             return 0;
+         if (n == 1 || n == 2) 
+             return 1;
+         return fibonacci(n - 1) + fibonacci(n - 2);
+}
+    
+    public static void main(String[] args) {
+          Scanner scanner = new Scanner (System. in);
+          int n = scanner. nextInt();
+          scanner.close();
+          if (n <= 0)
+                System.out.println(-1);
+          else{
+                 for (int i = 0; i < n; i++)
+                     System.out.print (fibonacci(i) + " ");
+           }
+}
