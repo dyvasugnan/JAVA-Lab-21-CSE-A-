85593495d69c9d86ad7ce42a22d8f7d360e6ff4d
21wh1a0536 <137029956+21wh1a0536@users.noreply.github.com>
2023-07-02 20:50:41 +0530

    Create Multilevel.java

---
+++ 21WH1A0536 - Class/Multilevel.java
@@ -0,0 +1,24 @@
+class First {
+int a;
+}
+class Parent extends First{
+	int b;
+}
+
+class Child extends Parent{
+	int c;
+	Child(){
+		a=10;
+		b=20;
+		c=30;
+	}
+	void sum() {
+		System.out.println("sum : "+(a+c+b));
+	}
+}
+public class Multilevel{
+	public static void main(String args[]) {
+		Child obj = new Child();
+		obj.sum();
+	}
+}
