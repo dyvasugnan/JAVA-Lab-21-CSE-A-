0b0f083914f70afa26889946cb05cf1b0be6731c
21wh1a0536 <137029956+21wh1a0536@users.noreply.github.com>
2023-07-02 20:20:27 +0530

    Create Add.java

---
+++ 21WH1A0536 - Class/Add.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Add{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        long n1 = sc.nextLong();
+        long n2 = sc.nextLong();
+        System.out.println("Addition : "+(n1+n2));
+    }
+}
