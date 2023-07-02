a4e06c6eed083001a85c203af79338c4193ad708
21wh1a0523 <137030067+21wh1a0523@users.noreply.github.com>
2023-07-02 21:50:22 +0530

    Add files via upload

---
+++ 523/EvenOdd.java
@@ -0,0 +1,14 @@
+package javaclss;
+import java.util.Scanner;
+public class EvenOdd {
+
+	public static void main(String[] args) {
+				Scanner sc = new Scanner(System.in);
+				System.out.print("Enter Number:");
+				int Num = sc.nextInt();
+				if(Num%2==0)
+				System.out.print("Even number\n");
+				else
+				System.out.print("Odd number\n");
+			}
+		}
