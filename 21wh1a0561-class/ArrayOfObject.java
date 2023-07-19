40badeccc5af63baa8a01b9cf2bc0c96b6e11d8b
Poojitha-61 <136248610+Poojitha-61@users.noreply.github.com>
2023-07-19 22:57:27 +0530

    Add files via upload

---
+++ 21wh1a0561-class/ArrayOfObject.java
@@ -0,0 +1,33 @@
+package javalab;
+import java.util.*;
+class Student{
+	int age;
+	String name;
+	Student(String name,int age){
+		this.name = name;
+		this.age = age;
+		
+	}
+	void display() {
+		System.out.println("Name : "+name);
+		System.out.println("Age  :"+age);
+	}
+}
+public class ArrayOfObject {
+	public static void main(String[] args) {
+		Student s[]= new Student[5];
+		String name;
+		int age;
+		Scanner sc = new Scanner(System.in);
+		for(int i = 0;i<5;i++) {
+			name = sc.next();
+			age = sc.nextInt();
+			s[i] = new Student(name,age);
+		}
+		for(int i = 0;i<5;i++) {
+			s[i].display();
+		}
+	}
+}	
+
+
