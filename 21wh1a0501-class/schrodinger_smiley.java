d135301a692998d32e06345518155c2a219592e2
21wh1a0501 <137029816+21wh1a0501@users.noreply.github.com>
2023-07-19 07:45:41 +0530

    Add files via upload

---
+++ 21wh1a0501-class/schrodinger_smiley.java
@@ -0,0 +1,80 @@
+/* package codechef; // don't place package name! */
+
+
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+
+/* Name of the class has to be "Main" only if the class is public. */
+
+class Codechef
+
+{
+
+	public static void main (String[] args) throws java.lang.Exception
+
+	{
+
+		// your code goes here
+
+		Scanner sc = new Scanner(System.in);
+
+		int t = sc.nextInt();
+
+		while(t>0){
+
+		    int n = sc.nextInt();
+
+		    String s= sc.next();
+
+		    int count = 0;
+
+		    String alpha = "";
+
+		    int i=0;
+
+		    if(s.charAt(0)==':'){
+
+		        alpha = ":";
+
+		        i=1;
+
+		    }
+
+		    for(; i<s.length(); i++){
+
+		        if(s.charAt(i)==':'){
+
+		            if(!alpha.contains("(") && alpha.contains(":") && alpha.contains(")")){
+
+		                count++;
+
+		            }
+
+		            alpha = ":";
+
+		        }
+
+		        else{
+
+		            alpha+=s.charAt(i);
+
+		        }
+
+		    }
+
+		    System.out.println(count);
+
+		    t--;
+
+		}
+
+	}
+
+}
+
