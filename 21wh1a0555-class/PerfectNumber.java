1fc22cad5bcd1b15b58338a22c6f79a259a1ada2
Ushashwini128 <136068686+Ushashwini128@users.noreply.github.com>
2023-07-19 22:06:25 +0530

    Add files via upload

---
+++ 21wh1a0555-class/PerfectNumber.java
@@ -0,0 +1,20 @@
+package hello;
+import java.util.*;
+class PerfectNumber {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int sum=0;
+		for(int i=1;i<num;i++) {
+			if(num%i==0) {
+				sum += i;
+			}
+		}
+		if(sum==num) {
+			System.out.println("It is a perfect number");
+		}
+		else {
+			System.out.println("It is not a perfect number");
+		}
+	}
+}
