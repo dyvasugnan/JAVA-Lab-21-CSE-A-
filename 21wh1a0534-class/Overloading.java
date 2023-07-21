02584a223bfe39185688bf607dbd511249e1623e
21wh1a0534 <137029742+21wh1a0534@users.noreply.github.com>
2023-07-21 22:39:51 +0530

    Add files via upload

---
+++ 21wh1a0534-class/Overloading.java
@@ -0,0 +1,22 @@
+package java_labcodes;
+
+class Calculate {
+		int sum(int x,int y) {
+			return (x+y);
+		}
+		int sum(int x,int y,int z) {
+			return (x+y+z);
+		}
+		double sum(double x,double y) {
+			return(x+y);
+		}
+
+}
+public class Overloading {
+	public static void main(String args[]) {
+		Calculate c= new Calculate();
+		System.out.println(c.sum(2, 3));
+		System.out.println(c.sum(2, 3, 5));
+		System.out.println(c.sum(2.5, 3.6));
+	}
+}
