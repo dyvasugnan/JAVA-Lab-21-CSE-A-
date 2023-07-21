f3ab36512ff79a55ab5ae8e76669f82c63705ad7
21wh1a0534 <137029742+21wh1a0534@users.noreply.github.com>
2023-07-21 22:40:50 +0530

    Add files via upload

---
+++ 21wh1a0534-class/Armstrong.java
@@ -0,0 +1,26 @@
+package java_labcodes;
+import java.util.*;
+import java.lang.*;
+public class Armstrong {
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter number");
+		int num = sc.nextInt();
+		int rem=0;
+		int res=0;
+		int temp=num;
+		while(num!=0) {
+			rem=num%10;
+			res+=Math.pow(rem,3);
+			num=num/10;
+			}
+		if(res==temp) {
+			System.out.print(temp+" "+"is a armstrong number");
+		}
+		else {
+			System.out.print(temp+" "+"is not a armstrong number");
+		}
+	}
+
+}
