56bfa3c8561de573bb5f767aa942877507e16126
21wh1a0526 <137029959+21wh1a0526@users.noreply.github.com>
2023-07-21 16:15:39 +0530

    Create ReverseNum.java

---
+++ 21wh1a0526 - Classwork/ReverseNum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+
+class ReverseNum {
+  public static void main(String[] args) {
+    int num = 2345, reversed = 0;
+    System.out.println("Original Number: " + num);
+    while(num != 0) {
+      int digit = num % 10;
+      reversed = reversed * 10 + digit;
+      num /= 10;
+    }
+    System.out.println("Reversed Number: " + reversed);
+  }
+}
