394444326af2dead0b2cf797729dd54b8fd9c04b
21wh1a0536 <137029956+21wh1a0536@users.noreply.github.com>
2023-07-23 23:36:38 +0530

    Create Example.java

---
+++ 21WH1A0536 - Class/Example.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class Example {  
+  
+    public static void main(String[] args) {  
+        try  
+        {  
+        int data=50/0;  
+        }  
+        catch(Exception e)  
+        {  
+            System.out.println(e);  
+        }  
+        //System.out.println("rest of the code");  
+    }  
+      
+} 
