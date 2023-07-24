471b7da336ebc3fcf45b1c01231524f9e70b8420
21wh1a0538 <137029577+21wh1a0538@users.noreply.github.com>
2023-07-24 06:38:36 +0530

    Add files via upload

---
+++ 21wh1a0538-classwork/WindowCreation.java
@@ -0,0 +1,16 @@
+package classprogram;
+import java.awt.*;
+import java.awt.event.*;
+class Myclass extends WindowAdapter{
+	public void windowclosing(WindowEvent we) {
+		windowClosed(we);
+	}
+}
+public class WindowCreation {
+	public static void main(String args[]) {
+  Frame f = new Frame();
+  f.setSize(400,400);
+  f.setTitle("First Window");
+  f.addWindowListener(new Myclass());
+  f.setVisible(true);
+}}
