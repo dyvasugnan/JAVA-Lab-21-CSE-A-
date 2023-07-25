93864789b63d94f2f4486574a16c5e4e05bfbf58
21wh1a0522 <121939368+21wh1a0522@users.noreply.github.com>
2023-07-25 20:10:56 +0530

    Add files via upload

---
+++ 522-classwork/CopyCons.java
@@ -0,0 +1,27 @@
+package javaclass;
+import java.util.*;
+class Test1{
+	String name;
+	int age;
+	Test1(String name,int age){
+		this.name = name;
+		this.age = age;
+	}
+	Test1(Test1 t){
+		age = t.age;
+		name = t.name;
+	}
+	void display() {
+		System.out.println("Name   : "+name);
+		System.out.println("Age    :"+age);
+	}
+}
+
+public class CopyCons {
+	  public static void main(String args[]) {
+		  Test1 t1 = new Test1("Meee",19);
+		  Test1 t2 = new Test1(t1);
+		  t1.display();
+		  t2.display();
+	  }
+}
