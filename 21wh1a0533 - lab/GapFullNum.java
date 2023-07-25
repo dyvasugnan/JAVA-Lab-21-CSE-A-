242f4f0c63ce1059c6af48ccda8d09f73e84dd5e
21wh1a0533 <137030356+21wh1a0533@users.noreply.github.com>
2023-07-25 20:32:55 +0530

    Add files via upload

---
+++ 21wh1a0533 - lab/GapFullNum.java
@@ -0,0 +1,25 @@
+package javaclass;
+import java.lang.*;
+import java.util.*;
+public class GapFullNum {
+		// TODO Auto-generated constructor stub
+			public static void main(String a[]){
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Enter the number : ");
+			int num = sc.nextInt();
+			int r=0,rem=0,temp=num;
+			r=num%10;
+			while(num>0){
+			rem =num%10;
+			num = num/10;
+			}
+			int res = (rem*10)+r;
+			System.out.println("First and last number are : "+res);
+			if(temp%res == 0)
+			System.out.print("Gapful number");
+			else
+			System.out.print("Not a Gapfull number");
+			}
+	}
+
+
