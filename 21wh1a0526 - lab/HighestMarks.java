903b49701c786132cc11f3a5a7c35ba00aca33e4
21wh1a0526 <137029959+21wh1a0526@users.noreply.github.com>
2023-07-25 21:00:52 +0530

    Create HighestMarks.java

---
+++ 21wh1a0526 - lab/HighestMarks.java
@@ -0,0 +1,29 @@
+//You are creating a student grading system. Each student has a name, student ID, and a list of marks for different subjects.
+//Implement a class in Java to represent a student and display the student’s name who is having highest score.
+
+import java.util.*;
+public class HighestMarks{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner sc = new Scanner(System.in);
+		 System.out.println("size:");
+	        int n=sc.nextInt();
+	       String name,stuName="";
+	       int id,marks,max=0,stuId=0,stuMarks=0;
+	  System.out.println("enter details: ");
+	       for(int i=0;i<n;i++){
+	           name = sc.next();
+	           id = sc.nextInt();
+	           marks = sc.nextInt();
+	           if(max<marks){
+	               max=marks;
+	               stuName=name;
+	               stuId=id;
+	               stuMarks=marks;
+	           }
+	       }
+	       System.out.print(stuName+"\n"+stuId+"\n"+stuMarks+"\n");
+	}
+
+}
