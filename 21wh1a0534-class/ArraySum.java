020959f3bc318faf3c7f25fe9861509ebb438e45
21wh1a0534 <137029742+21wh1a0534@users.noreply.github.com>
2023-07-25 22:47:23 +0530

    Add files via upload

---
+++ 21wh1a0534-class/ArraySum.java
@@ -0,0 +1,17 @@
+package java_labcodes;
+import java.util.*;
+class ArraySum{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter value of n ");
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        int sum=0;
+        System.out.print("enter the array elements ");
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+            sum+= a[i];
+        }
+        System.out.println("sum of array elements is " + sum);
+    }
+}
