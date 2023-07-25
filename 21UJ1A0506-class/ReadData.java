485c7ba444c700454efb4d928e59b4c7a29e2557
21UJ1A0506 <137258686+21UJ1A0506@users.noreply.github.com>
2023-07-25 18:45:15 +0530

    Add files via upload

---
+++ 21UJ1A0506-class/ReadData.java
@@ -0,0 +1,20 @@
+package practice.java;
+import java.lang.*;
+import java.util.*;
+
+public class ReadData {
+
+	public ReadData() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter a num");
+		int n = sc.nextInt();
+		System.out.print(n);
+
+	}
+
+}
