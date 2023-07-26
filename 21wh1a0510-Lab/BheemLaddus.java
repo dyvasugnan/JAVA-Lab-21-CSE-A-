7f813b0546f77468e8d95a5466c6d06344b9b507
21wh1a0510 <121952199+21wh1a0510@users.noreply.github.com>
2023-07-26 07:40:23 +0530

    Create BheemLaddus.java

---
+++ 21wh1a0510-Lab/BheemLaddus.java
@@ -0,0 +1,25 @@
+import java.lang.*;
+import java.util.*;
+
+public class BheemLaddus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] laddus = sc.nextLine().split(",");
+        int n = laddus.length;
+        int maxLaddus = n / 2;
+        int[] count = new int[100];
+        int types = 0;
+        for (String laddu : laddus) {
+            int type = Integer.parseInt(laddu);
+            if (count[type] == 0) {
+                types++;
+            }
+            count[type]++;
+        }
+        if (types <= maxLaddus) {
+            System.out.println(types);
+        } else {
+            System.out.println(maxLaddus);
+        }
+    }
+}
