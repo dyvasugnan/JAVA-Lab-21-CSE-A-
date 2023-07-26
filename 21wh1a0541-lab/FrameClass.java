37f9debe43cb81a700cbe878433c265e30501182
21wh1a0541 <123742371+CHNITHISHA@users.noreply.github.com>
2023-07-26 15:14:38 +0530

    Add files via upload

--- 21wh1a0541-lab/FrameClass.java
+++ 21wh1a0541-lab/FrameClass.java
@@ -1,33 +1,67 @@
-package java21541;
-import java.awt.*;
+package gui;
+import java.awt.*;// abstract window toolkit
 import java.awt.event.*;
-public class FrameClass extends Frame{
-public void paint(Graphics g) {
-this.setBackground(Color.cyan);
-//g.drawRect(100, 100, 200, 200);
-//g.setColor(Color.blue);
-g.fillRect(100, 100, 200, 200);
-g.drawOval(100, 100, 200, 200);
-g.setColor(Color.yellow);
-g.fillOval(100, 100, 200, 200);
-g.setColor(Color.black);
-g.fillOval(150,150,25,25);
-g.fillOval(230,150,25,25);
-g.fillRect(200, 190, 10, 25);
-g.fillRect(165, 250, 75, 15);
-
-}
-public static void main(String[] args) {
-FrameClass f = new FrameClass();
-f.setTitle("Test");
-f.setSize(400,400);
-f.setVisible(true);
-f.addWindowListener(new Myclass());
-}
-}
-class Myclass extends WindowAdapter{
-public void windowClosing(WindowEvent we) {
-System.exit(0);
-}
-
+public class FrameClass extends Frame implements ActionListener {
+		Button b1,b2;
+		TextField n1,n2,res;
+		Label num1,num2,result;
+		FrameClass(){
+			this.setLayout(null);
+			b1 = new Button("Add");
+		    b2 = new Button("Clear");
+			num1 = new Label("Number 1");
+			num2 = new Label("Number 2");
+			result = new Label("Result");
+			n1 = new TextField();
+			n2 = new TextField();
+			res = new TextField();
+			num1.setBounds(100, 150, 100, 50);
+			n1.setBounds(200, 150, 100, 50);
+			
+			num2.setBounds(100, 250, 100, 50);
+			n2.setBounds(200, 250, 100, 50);
+			
+			result.setBounds(100, 350, 100, 50);
+			res.setBounds(200, 350, 100, 50);
+			b1.setBounds(150, 450, 100, 50);
+			b2.setBounds(250,450,100,50);
+			this.add(b1);
+			this.add(b2);
+			this.add(num1);
+			this.add(num2);
+			this.add(result);
+			this.add(n1);
+			this.add(n2);
+			this.add(res);
+			b1.addActionListener(this);
+			b2.addActionListener(this);
+			n1.addActionListener(this);
+			n2.addActionListener(this);
+			res.addActionListener(this);
+		}
+		public void actionPerformed(ActionEvent ae) {
+			String str = ae.getActionCommand();
+			if(str == "Add") {
+				int a = Integer.parseInt(n1.getText());
+				int b = Integer.parseInt(n2.getText());
+				int c = a+b ;
+				res.setText(String.valueOf(c));
+			}
+			else if(str == "Clear") {
+				n1.setText("");
+				n2.setText("");
+				res.setText("");
+			}
+		}
+	public static void main(String[] args) {
+		FrameClass f = new FrameClass();
+		f.setTitle("Test");
+		f.setSize(400, 400);
+		f.setVisible(true);
+		f.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent we) {
+				System.exit(0);
+			}
+		});
+	}
 }
