a533ff80f977ce856991ab7136c80bbec67a4fe6
21wh1a0513 <122883244+Akshaya21513@users.noreply.github.com>
2023-07-26 15:57:29 +0530

    Add files via upload

---
+++ 513-lab/GuiAdd.java
@@ -0,0 +1,79 @@
+package javalab;
+import java.awt.*;// abstract window toolkit
+import java.awt.event.*;
+public class GuiAdd extends Frame implements ActionListener {
+
+		// TODO Auto-generated method stub
+		
+		
+				Button b1,b2;
+				TextField n1,n2,res;
+				Label num1,num2,result;
+				GuiAdd(){
+					this.setLayout(null);
+					b1 = new Button("Add");
+					b2 = new Button("ClearAll");
+					
+					num1 = new Label("Number 1");
+					num2 = new Label("Number 2");
+					result = new Label("Result");
+					
+					n1 = new TextField();
+					n2 = new TextField();
+					res = new TextField();
+					
+					num1.setBounds(100, 150, 100, 50);
+					n1.setBounds(200, 150, 100, 50);
+					
+					num2.setBounds(100, 250, 100, 50);
+					n2.setBounds(200, 250, 100, 50);
+					
+					result.setBounds(100, 350, 100, 50);
+					res.setBounds(200, 350, 100, 50);
+					
+					b1.setBounds(150, 450, 100, 50);
+					b2.setBounds(250, 450, 100, 50);
+					
+					this.add(b1);
+					this.add(b2);
+					this.add(num1);
+					this.add(num2);
+					this.add(result);
+					this.add(n1);
+					this.add(n2);
+					this.add(res);
+					
+					b1.addActionListener(this);
+					b2.addActionListener(this);
+					n1.addActionListener(this);
+					n2.addActionListener(this);
+					res.addActionListener(this);
+				}//con
+				
+				public void actionPerformed(ActionEvent ae) {
+					String str = ae.getActionCommand();
+					if(str == "Add") {
+						int a = Integer.parseInt(n1.getText());
+						int b = Integer.parseInt(n2.getText());
+						int c = a+b ;
+						res.setText(String.valueOf(c));
+					}else if(str == "ClearAll"){ //can use str.equals("ClearAll")
+						n1.setText("");
+						n2.setText("");
+						res.setText("");
+					}
+					}
+				
+			public static void main(String[] args) {
+				GuiAdd f = new GuiAdd();
+				f.setTitle("Adding numbers ");
+				f.setSize(400, 600);
+				f.setVisible(true);
+				f.addWindowListener(new WindowAdapter() {
+					public void windowClosing(WindowEvent we) {
+						System.exit(0);
+					}
+				});
+			}
+		}
+
