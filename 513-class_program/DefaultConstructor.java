c5fb090889399c2b4e40688d64e229e5b7f2e37a
21wh1a0513 <122883244+Akshaya21513@users.noreply.github.com>
2023-07-26 08:39:26 +0530

    Add files via upload

---
+++ 513-class_program/DefaultConstructor.java
@@ -0,0 +1,16 @@
+package javalab;
+
+public class DefaultConstructor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Call c = new Call();
+		
+	}
+
+}
+class Call{
+	Call(){
+		System.out.println("Default constructor called");
+	}
+}
