93f112873b18c6e17a5a82683a2da0dff8d2f1a7
21wh1a0541 <123742371+CHNITHISHA@users.noreply.github.com>
2023-07-26 22:36:18 +0530

    Add files via upload

--- 21wh1a0541-lab/FrameClass.java
+++ 21wh1a0541-lab/FrameClass.java
@@ -1,62 +1,35 @@
-package guijava;
-import java.awt.*;// abstract window toolkit
+package javaprograms;
+
+import java.awt.*;
+// abstract window toolkit
 import java.awt.event.*;
 public class FrameClass extends Frame implements ActionListener {
-		Button b1,b2;
-		TextField n1,n2,res;
-		Label num1,num2,result;
+		Button b1,b2,b3;
 		FrameClass(){
 			this.setLayout(null);
-			b1 = new Button("Div");
-		    b2 = new Button("Clear");
-			num1 = new Label("Number 1");
-			num2 = new Label("Number 2");
-			result = new Label("Result");
-			n1 = new TextField();
-			n2 = new TextField();
-			res = new TextField();
-			num1.setBounds(100, 150, 100, 50);
-			n1.setBounds(200, 150, 100, 50);
-			
-			num2.setBounds(100, 250, 100, 50);
-			n2.setBounds(200, 250, 100, 50);
-			
-			result.setBounds(100, 350, 100, 60);
-			res.setBounds(200, 350, 100, 50);
-			b1.setBounds(150, 450, 100, 50);
-			b2.setBounds(250,450,100,50);
+			b1 = new Button("Red");
+			b2 = new Button("Blue");
+			b3 = new Button("Green");
+			b1.setBounds(100, 150, 100, 50);
+			b2.setBounds(100, 250, 100, 50);
+			b3.setBounds(100, 350, 100, 50);
 			this.add(b1);
 			this.add(b2);
-			this.add(num1);
-			this.add(num2);
-			this.add(result);
-			this.add(n1);
-			this.add(n2);
-			this.add(res);
+			this.add(b3);
 			b1.addActionListener(this);
 			b2.addActionListener(this);
-			n1.addActionListener(this);
-			n2.addActionListener(this);
-			res.addActionListener(this);
-		}
+			b3.addActionListener(this);
+					}
 		public void actionPerformed(ActionEvent ae) {
 			String str = ae.getActionCommand();
-			if(str == "Div") {
-				int a = Integer.parseInt(n1.getText());
-				int b = Integer.parseInt(n2.getText());
-				if(b==0) {
-					res.setText("Error:division by 0");
-				}
-				else {
-				int c = a/b ;
-				res.setText(String.valueOf(c));
-				}
-			}
-			else if(str == "Clear") {
-				n1.setText("");
-				n2.setText("");
-				res.setText("");
-			}
+			if(str == "Red")
+					this.setBackground(Color.RED);
+			if(str == "Blue")
+				this.setBackground(Color.BLUE);
+		
+			if(str == "Green")
+				this.setBackground(Color.GREEN);
+		
 		}
 	public static void main(String[] args) {
 		FrameClass f = new FrameClass();
