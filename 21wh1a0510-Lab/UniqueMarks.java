5194af6462dd0ac94521517f88b99f3a7432a70f
21wh1a0510 <121952199+21wh1a0510@users.noreply.github.com>
2023-07-26 08:33:58 +0530

    Update UniqueMarks.java

--- 21wh1a0510-Lab/UniqueMarks.java
+++ 21wh1a0510-Lab/UniqueMarks.java
@@ -1,37 +1,38 @@
-import java.lang.*;
 import java.util.*;
 public class UniqueMarks {
-public static void main(String args[]) {
-Scanner sc = new Scanner(System.in);
-int n = sc.nextInt();
-int a[] = new int[n];
-int c[] = new int[n];
-int count =0;
-for(int i=0;i<n;i++)
-a[i]= sc.nextInt();
-for(int i=0;i<n;i++) {
-for(int j=0;j<n;j++) {
-if(i!=j) {
-if(a[i]!=a[j]) {
-c[i]=a[i];
-}
-else {
-count++;
-break;
-}
-}
-}
-}
-int max = c[0];
-if(count == n)
-System.out.print("all are disqualified");
-else if(count<n) {
-for(int k=0;k<n;k++) {
-if(max<c[k])
-max=c[k];
-}
-System.out.print("Max element is: "+max);
-}
+	public static void main(String args[]) {
+		int n,k=0;
+		System.out.print("Enter the number of students");
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		int a[] = new int[n];
+		int i,j,count=1;
+		System.out.print("Enter the marks of each student : ");
+		for(i = 0 ;i<n;i++)
+		a[i] = sc.nextInt();
+		int max;
+		int eq = a[0];
+		int uni[] = new int[n];
+		for(i = 0;i<n;i++){
+		count = 0;
+		for(j = 0;j<n;j++) {
+		if(a[i]==a[j])
+		count ++;
+		}
+		uni[k] = count;
+		k++;
+		}
+		max = 0;
+		for(i = 0;i<n;i++){
+		if(uni[i]==1) {
+		if(a[i] >max)
+		max = a[i];
+		} }
+		if(max==0)
+		System.out.print(-1);
+		else
+		System.out.println(max);
 
-}
+		}
+
 }
