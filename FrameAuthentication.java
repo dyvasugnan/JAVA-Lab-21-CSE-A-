908e1ac54d9b2f99e031746c47987ff5f03fae1f
22wh5a0502 <137030538+22wh5a0502@users.noreply.github.com>
2023-07-28 20:26:28 +0530

    Add files via upload

---
+++ FrameAuthentication.java
@@ -0,0 +1,44 @@
+package javalab;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class FrameAuthentication extends Frame implements ActionListener {
+		// TODO Auto-generated method stupublic class FrameClass extends Frame implements ActionListener {
+		TextField name,pass;
+		Label l1,l2;
+		FrameAuthentication(){
+			this.setLayout(new FlowLayout());
+			name = new TextField("user name",20);
+			pass  = new TextField(20);
+			pass.setEchoChar('*');
+			l1 = new Label("user name");
+			l2 = new Label("pwd",Label.LEFT);
+			add(l1);
+			add(name);
+			add(l2);
+			add(pass);
+			name.addActionListener(this);
+			pass.addActionListener(this);
+				}
+		public void actionPerformed(ActionEvent ae) {
+			Graphics g = this.getGraphics();
+			g.drawString("User Name: "+name.getText(),100,100);
+			g.drawString( "Pwd: "+pass.getText(),100,200);
+			
+				
+			}
+		
+	
+	public static void main(String[] args) {
+		FrameClass f = new FrameClass();
+		f.setTitle("Test");
+		f.setSize(400, 400);
+		f.setVisible(true);
+		f.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent we) {
+				System.exit(0);
+			}
+		});
+	}
+	}
