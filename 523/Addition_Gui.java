2e2a9725ecf6369214ce2c2e604b0377bd17766f
21wh1a0523 <137030067+21wh1a0523@users.noreply.github.com>
2023-07-29 13:07:28 +0530

    Add files via upload

---
+++ 523/Addition_Gui.java
@@ -0,0 +1,71 @@
+package labprograms;
+import java.awt.event.*;
+import java.awt.*;
+public class Addition_Gui extends Frame implements ActionListener {
+	
+	Button b1,c1;
+	TextField n1, n2, res;
+	Label num1, num2, result;
+	public Addition_Gui() {
+		this.setLayout(null);
+		b1 = new Button("Add");
+		c1 = new Button("CLEAR");
+		num1 = new Label("Number 1 ");
+		num2 = new Label("Number 2 ");
+		result = new Label("Result ");
+		 n1 = new TextField();
+		 n2 = new TextField();
+		 res = new TextField();
+		 num1.setBounds(100, 150, 100, 50);
+		 n1.setBounds(200, 150, 100, 50);
+
+		 num2.setBounds(100, 250, 100, 50);
+		 n2.setBounds(200, 250, 100, 50);
+		 
+		 result.setBounds(100, 350, 100, 50);
+		 res.setBounds(200, 350, 100, 50);
+		 
+		 b1.setBounds(150, 450, 100, 50);
+		 c1.setBounds(150, 550, 100, 50);
+		 
+		 this.add(b1);
+		 this.add(num1);
+		 this.add(num2);
+		 this.add(result);
+		 this.add(n1);
+		 this.add(n2);
+		 this.add(res); 
+		 this.add(c1);
+		 b1.addActionListener(this);
+		 n1.addActionListener(this);
+		 n2.addActionListener(this);
+		 res.addActionListener(this);
+		 c1.addActionListener(this);
+	}
+	public void actionPerformed(ActionEvent ae) {
+		String str = ae.getActionCommand();
+		if(str == "Add") {
+			int a =  Integer.parseInt(n1.getText());
+			int b =  Integer.parseInt(n2.getText());
+			int c = a+ b;
+			res.setText(String.valueOf(c));
+		}
+		if(str == "CLEAR") {
+			n1.setText("");
+			n2.setText("");
+			res.setText("");
+		}
+	}
+	public static void main(String[] args) {
+		Addition_Gui f = new Addition_Gui();
+		f.setTitle("Addition");
+		f.setBackground(Color.gray);
+		f.setSize(400, 400);
+		f.setVisible(true);
+		f.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent we) {
+				System.exit(0);
+			}
+		});
+	}
+}
