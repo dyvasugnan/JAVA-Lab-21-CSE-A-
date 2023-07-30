1be0aea141a17a95788d8a818ed682c550526d94
21wh1a0519 <137029965+21wh1a0519@users.noreply.github.com>
2023-07-30 10:00:44 +0530

    Add files via upload

---
+++ 519-Lab/SpyNumber.java
@@ -0,0 +1,25 @@
+package java_codes;
+import java.lang.*;
+import java.util.*;
+public class SpyNumber {
+	public static void main(String args[]){
+		int n;
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		int sum = 0;
+		int product = 1;
+		while(n > 0){
+		int rem = n % 10;
+		sum += rem;
+		product *= rem;
+		n  = n / 10;
+		}
+		if(sum == product){
+		System.out.println("YES");
+		}
+		else{
+		System.out.println("NO");
+		}
+		}
+
+}
