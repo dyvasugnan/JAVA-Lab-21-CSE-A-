cdb540045ed2bde7f919a03e3771246d3d2332e1
21wh1a0522 <121939368+21wh1a0522@users.noreply.github.com>
2023-07-30 17:32:59 +0530

    Create DupChar.java

---
+++ 522-lab/DupChar.java
@@ -0,0 +1,21 @@
+package javaclass;
+import java.util.*;
+public class DupChar {
+	  public static String dupliRemove(String s) {
+	        if (s.length() < 2) {
+	            return s;
+	        }
+	        if (s.charAt(0) == s.charAt(1)) {
+	            return dupliRemove(s.substring(1));
+	        } else {
+	            return s.charAt(0) + dupliRemove(s.substring(1));
+	        }
+	    }
+
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter a string: ");
+	        String s = sc.nextLine();
+	        System.out.println(dupliRemove(s));
+	    }
+}
