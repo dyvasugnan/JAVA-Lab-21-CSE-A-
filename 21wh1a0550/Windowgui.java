42a47fda995b04ccfbc99336a02e4782e2dfb16a
N-sindura2003 <137538746+N-sindura2003@users.noreply.github.com>
2023-07-30 22:46:00 +0530

    Add files via upload

---
+++ 21wh1a0550/Windowgui.java
@@ -0,0 +1,18 @@
+package javaclasswork;
+import java.awt.*;
+import java.awt.event.*;
+	
+class Myclass extends WindowAdapter{
+		public void windowclosing(WindowEvent we) {
+			windowClosed(we);
+		}
+	}
+	public class Windowgui {
+		public static void main(String args[]) {
+	  Frame f = new Frame();
+	  f.setSize(400,400);
+	  f.setTitle("First Window");
+	  f.addWindowListener(new Myclass());
+	  f.setVisible(true);
+	}
+	}
