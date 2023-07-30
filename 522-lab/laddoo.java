23af573a0f27b6862b2fd1c4b4823ff05c717e35
21wh1a0522 <121939368+21wh1a0522@users.noreply.github.com>
2023-07-30 16:28:49 +0530

    Create laddoo.java

---
+++ 522-lab/laddoo.java
@@ -0,0 +1,29 @@
+package javalab;
+import java.util.*;
+
+public class laddoo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String[] lado = sc.nextLine().split(",");
+		int n = lado.length;
+		int maxLaddus = n / 2;
+		int[] count = new int[50]; 
+		int unique = 0;
+		for (String i : lado) {
+			int type = Integer.parseInt(i);
+			if (count[type] == 0) {
+			     unique++;
+			}
+			    count[type]++;
+			}
+			if (unique <= maxLaddus) {
+			    System.out.println(unique);
+			} else {
+				System.out.println(maxLaddus);
+			}
+
+	}
+
+}
