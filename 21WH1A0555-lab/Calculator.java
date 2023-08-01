de7887344c3e759241fa65a204230180a35cd6f9
Ushashwini128 <136068686+Ushashwini128@users.noreply.github.com>
2023-08-01 22:30:22 +0530

    Add files via upload

---
+++ 21WH1A0555-lab/Calculator.java
@@ -0,0 +1,130 @@
+import java.awt.*;
+import java.awt.event.*;
+
+	public class Cal extends Frame implements ActionListener {
+	    Button b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16;
+	    TextField num;
+	    String operator = "";
+	    int num1, num2, result;
+
+	    CalGui() {
+	        num = new TextField();
+	        num.setBounds(50, 150, 300, 50);
+	        this.add(num);
+	        b1 = new Button("1");
+	        b2 = new Button("2");
+	        b3 = new Button("3");
+	        b4 = new Button("4");
+	        b5 = new Button("5");
+	        b6 = new Button("6");
+	        b7 = new Button("7");
+	        b8 = new Button("8");
+	        b9 = new Button("9");
+	        b10 = new Button("0");
+	        b11 = new Button("/");
+	        b12 = new Button("*");
+	        b13 = new Button("+");
+	        b14 = new Button("-");
+	        b15 = new Button("=");
+	        b16 = new Button("c");
+
+	        b1.addActionListener(this);
+	        b2.addActionListener(this);
+	        b3.addActionListener(this);
+	        b4.addActionListener(this);
+	        b5.addActionListener(this);
+	        b6.addActionListener(this);
+	        b7.addActionListener(this);
+	        b8.addActionListener(this);
+	        b9.addActionListener(this);
+	        b10.addActionListener(this);
+	        b11.addActionListener(this);
+	        b12.addActionListener(this);
+	        b13.addActionListener(this);
+	        b14.addActionListener(this);
+	        b15.addActionListener(this);
+	        b16.addActionListener(this);
+
+	        this.add(b1);
+	        this.add(b2);
+	        this.add(b3);
+	        this.add(b4);
+	        this.add(b5);
+	        this.add(b6);
+	        this.add(b7);
+	        this.add(b8);
+	        this.add(b9);
+	        this.add(b10);
+	        this.add(b11);
+	        this.add(b12);
+	        this.add(b13);
+	        this.add(b14);
+	        this.add(b15);
+	        this.add(b16);
+	        b1.setBounds(50, 200, 50, 50);
+	        b2.setBounds(120, 200, 50, 50);
+	        b3.setBounds(190, 200, 50, 50);
+	        b11.setBounds(260, 200, 50, 50);
+	        b4.setBounds(50, 270, 50, 50);
+	        b5.setBounds(120, 270, 50, 50);
+	        b6.setBounds(190, 270, 50, 50);
+	        b12.setBounds(260, 270, 50, 50);
+	        b7.setBounds(50, 340, 50, 50);
+	        b8.setBounds(120, 340, 50, 50);
+	        b9.setBounds(190, 340, 50, 50);
+	        b10.setBounds(260, 340, 50, 50);
+	        b13.setBounds(50, 410, 50, 50);
+	        b14.setBounds(120, 410, 50, 50);
+	        b15.setBounds(190, 410, 50, 50);
+	        b16.setBounds(260, 410, 50, 50);
+
+	        this.setTitle("Calculator");
+	        this.setSize(400, 500);
+	        this.setLayout(null);
+	        this.setVisible(true);
+	    }
+
+	    public void actionPerformed(ActionEvent e) {
+	        String s = e.getActionCommand();
+
+	        if (Character.isDigit(s.charAt(0))) {
+	            num.setText(num.getText() + s);
+	        } else if (s.charAt(0) == 'c') {
+	            num.setText("");
+	            num1 = num2 = result = 0;
+	            operator = "";
+	        } else if (s.charAt(0) == '=') {
+	            num2 = Integer.parseInt(num.getText());
+	            switch (operator) {
+	                case "+":
+	                    result = num1 + num2;
+	                    break;
+	                case "-":
+	                    result = num1 - num2;
+	                    break;
+	                case "*":
+	                    result = num1 * num2;
+	                    break;
+	                case "/":
+	                        result = num1 / num2;
+	                        break;
+
+	            }
+	            num.setText(Integer.toString(result));
+	            num1 = result;
+	            operator = "";
+	        } else {
+	            num1 = Integer.parseInt(num.getText());
+	            operator = s;
+	            num.setText("");
+	        }
+	    }
+
+	    public static void main(String[] args) {
+	        new CalGui().addWindowListener(new WindowAdapter() {
+	            public void windowClosing(WindowEvent we) {
+	                System.exit(0);
+	            }
+	        });
+	    }
+	}
