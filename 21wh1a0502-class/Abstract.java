7e7256cdff2cebdf8b72b8204b3030f183dfa821
21wh1a0502 <137030074+21wh1a0502@users.noreply.github.com>
2023-08-03 12:57:13 +0530

    Add files via upload

---
+++ 21wh1a0502-class/Abstract.java
@@ -0,0 +1,17 @@
+
+package javalab;
+abstract class Bike{
+	abstract void run();	
+}
+class Pulsar extends Bike{
+	void run() {
+		System.out.println("running safely");
+	}
+}
+ class Abstract {
+
+	public static void main(String[] args) {
+		Bike obj = new Pulsar();
+		obj.run();
+	}
+}
