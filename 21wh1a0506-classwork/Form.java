7828df9faa393723dffb5c15fbb20539aa762b18
21WH1A0506 <137029990+21WH1A0506@users.noreply.github.com>
2023-08-03 20:42:21 +0530

    Create Form.java

---
+++ 21wh1a0506-classwork/Form.java
@@ -0,0 +1,31 @@
+package javaclass;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Form implements ItemListener {
+    Form() {
+        Frame frame = new Frame();
+        frame.setSize(400, 400);
+        frame.setLayout(null);
+        frame.setVisible(true);
+
+        Choice departmentChoice = new Choice();
+        departmentChoice.setBounds(100, 150, 200, 250);
+        departmentChoice.add("Computer Science");
+        departmentChoice.add("Information Technology");
+        departmentChoice.add("Electrical and Electronics");
+        departmentChoice.add("Electronics and Communication");
+        departmentChoice.add("Computer Science and Business Systems");
+
+        frame.add(departmentChoice);
+
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
+                System.exit(0);
+            }
+        });
+    }
+    public static void main(String args[]) {
+        new Form();
+    }
+}
