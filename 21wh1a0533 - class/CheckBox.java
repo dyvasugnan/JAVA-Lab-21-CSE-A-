a89766eb06b1586e23dac20c85b24551d77ddd42
21wh1a0533 <137030356+21wh1a0533@users.noreply.github.com>
2023-08-03 20:34:00 +0530

    Add files via upload

---
+++ 21wh1a0533 - class/CheckBox.java
@@ -0,0 +1,41 @@
+package javaclass;
+import java.awt.*;
+import java.awt.event.*;
+public class CheckBox extends Frame implements ActionListener,ItemListener{
+Button b1;
+Checkbox cb1;
+CheckBox(){
+	this.setLayout(new FlowLayout());
+	b1 = new Button("RED");
+	b1.setBounds(100,150,100,50);
+	this.add(b1);
+	b1.addActionListener(this);
+	cb1 = new Checkbox("GREEN");
+	add(cb1);
+	cb1.addItemListener(this);	
+}
+public void itemStateChanged(ItemEvent ie) {
+	if (cb1.getState()) {
+        
+        setBackground(Color.GREEN);
+    }
+	repaint();
+}
+public void actionPerformed(ActionEvent ae) {
+	String str = ae.getActionCommand();
+	if(str == "RED")
+			this.setBackground(Color.RED);
+	
+}
+public static void main(String[] args) {
+	CheckBox cb = new CheckBox();
+	cb.setTitle("Test");
+	cb.setSize(400, 400);
+	cb.setVisible(true);
+	cb.addWindowListener(new WindowAdapter() {
+		public void windowClosing(WindowEvent we) {
+			System.exit(0);
+		}
+	});
+}
+}
