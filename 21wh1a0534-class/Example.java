acab0795cf1fab3cdfd107ba3495747f75fe2a28
21wh1a0534 <137029742+21wh1a0534@users.noreply.github.com>
2023-08-03 11:33:44 +0530

    Create Example.java

---
+++ 21wh1a0534-class/Example.java
@@ -0,0 +1,28 @@
+package javaclass;
+import java.awt.*;    
+import java.awt.event.*;   
+
+public class Example implements ItemListener {
+	choicee(){
+		Frame f =new Frame();
+		f.setSize(400,400);
+	  f.setLayout(null);    
+	  f.setVisible(true); 
+		Choice dept=new Choice();
+		dept.setBounds(100,150,200,250);
+		dept.add("CSE");
+		dept.add("IT");
+		dept.add("EEE");
+		dept.add("ECE");
+		dept.add("CSBS");
+    f.add(dept);	
+		f.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent we) {
+				System.exit(0);
+			}
+		});
+	}
+	public static void main(String args[])    {    
+	   new Example();    
+	}    
+	} 
