22e6d75e7ae18e7384dfd1cdd3b175d49f614878
anusha-547 <136190129+anusha-547@users.noreply.github.com>
2023-08-06 12:06:07 +0530

    Add files via upload

---
+++ 21wh1a0547-lab/ChottaBheem.java
@@ -0,0 +1,36 @@
+package java_lab;
+
+public class ChottaBheem{
+	    public static void main(String [] args){
+	        int arr[]=new int[]{2,4,1,2,3,4};
+	        int []ct = new int[arr.length];
+	        int visited = -1;
+	        int flag=0,ans=-1;
+	        for(int i=0;i<arr.length;i++){
+	            int count=1;
+	            for(int j = i+1;j<arr.length;j++){
+	                if(arr[i] == arr[j]){
+	                    count++;
+	                    ct[j] = visited;
+	                }
+	            }
+	            if(ct[i] != visited){
+	                ct[i] = count;
+	            }
+	           
+	        }
+	        for(int i=0;i<arr.length;i++){
+	            if(ct[i]>0){
+	                flag+=1;
+	            }
+	        }
+	        if(flag>arr.length/2){
+	            System.out.println(arr.length/2);
+	        }
+	        else{
+	            System.out.println(flag);
+	        }
+	    }
+	}
+
+
