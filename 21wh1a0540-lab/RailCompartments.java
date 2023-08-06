7cd8d89b0bc289729a963d4ce3c307b8a1e0ae05
21wh1a0540 <137029580+21wh1a0540@users.noreply.github.com>
2023-08-07 05:12:18 +0530

    RailCompartments.java

--- 21wh1a0540-lab/RailCompartments.java
+++ 21wh1a0540-lab/RailCompartments.java
@@ -1,5 +1,26 @@
 package javalab.day19062023;
-
+import java.util.*;
 public class RailCompartments {
-
+public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int a[] = new int[n];
+		for(int i=0;i<n;i++){
+			a[i] = sc.nextInt();
+		}
+		Arrays.sort(a);
+		int long_seq = 0;
+		int curr_seq = 1;
+		for(int i=1;i<a.length;i++){
+			if(a[i]!=a[i-1]){
+				if(a[i]== a[i-1]+1){
+					curr_seq++;
+				}
+				else{
+					curr_seq = 1;
+				}
+				long_seq = Math.max(long_seq,curr_seq);
+			}
+	}
+	System.out.println(long_seq);
 }
