f1f3c327f8b03c2853f49516fe386f7a7c1f7a19
21wh1a0538 <137029577+21wh1a0538@users.noreply.github.com>
2023-08-06 18:28:33 +0530

    Add files via upload

---
+++ 21wh1a0538-lab/ConatString.java
@@ -0,0 +1,12 @@
+package classprogram;
+import java.util.*;
+public class ConatString {
+	public static void main(String args[]) {
+	Scanner sc = new Scanner(System.in);
+    String s1,s2;
+    s1 = sc.next();
+    s2 = sc.next();
+    String s3 = s1.concat(s2);
+    System.out.print(s3);
+    
+}}
