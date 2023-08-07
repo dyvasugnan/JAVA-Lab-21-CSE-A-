bc895ca73dc92645646aac9d62773a91c7942e42
21wh1a0523 <137030067+21wh1a0523@users.noreply.github.com>
2023-08-07 07:55:11 +0530

    Add files via upload

---
+++ 523/Student.java
@@ -0,0 +1,31 @@
+package javalab;
+import java.io.*;
+public class Student {
+	public static void main(String args[])throws IOException{
+		
+	InputStreamReader isr = new InputStreamReader(System.in);
+	BufferedReader br = new BufferedReader(isr);
+
+	System.out.print("Enter name    : ");
+	String name =br.readLine();
+
+	System.out.print("Enter age     : ");
+	String a = br.readLine();
+	int age = Integer.parseInt(a);
+
+	System.out.print("Enter gender  : ");
+	String gender = br.readLine();
+
+	System.out.print("Enter marks : ");
+	int marks = Integer.parseInt(br.readLine());
+
+	System.out.print("Enter address : ");
+	String address = br.readLine();
+
+	System.out.println("Name      :"+name);
+	System.out.println("Age       :"+age);
+	System.out.println("Marks     :"+marks);
+	System.out.println("Gender    :"+gender);
+	System.out.println("Address   :"+address);
+	}
+}
