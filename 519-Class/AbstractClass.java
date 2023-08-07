cf597eb198cb064256b82ece7b6122a0448a061a
21wh1a0519 <137029965+21wh1a0519@users.noreply.github.com>
2023-08-07 20:07:57 +0530

    Add files via upload

---
+++ 519-Class/AbstractClass.java
@@ -0,0 +1,38 @@
+package java_codes;
+abstract class Shape {
+    public abstract int area();
+    }
+
+class Circle extends Shape {
+     int  radius;
+
+     Circle(int radius) {
+        this.radius = radius;
+    }
+
+    public int area() {
+        return (int)(3.14* radius * radius);
+    }
+}
+class Rectangle extends Shape {
+    int  length;
+    int  breadth;
+
+    public Rectangle(int  length,int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    public int area() {
+        return length * breadth;
+    }
+}
+
+public class AbstractClass {
+    public static void main(String[] args) {
+        Shape circle = new Circle(6);
+        Shape rectangle = new Rectangle(9, 5);
+        System.out.println("Circle area" + circle.area());
+        System.out.println("Rectangle area" + rectangle.area());
+    }
+}
