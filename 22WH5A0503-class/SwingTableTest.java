8d37f543ca8dbf7824e4d73cb35a28a76e05bc2a
Gouni sony <137030608+Gounisony@users.noreply.github.com>
2023-08-07 21:27:37 +0530

    Add files via upload

---
+++ 22WH5A0503-class/SwingTableTest.java
@@ -0,0 +1,45 @@
+package javalab;
+import java.awt.*;
+import java.util.Vector;
+import javax.swing.*;
+import javax.swing.table.JTableHeader;
+public class SwingTableTest extends JFrame{
+    JTable tab;
+    SwingTableTest(){
+        Vector<Vector> data=new Vector<Vector>();
+        Vector<String> row=new Vector<String>();
+        row.add("201");
+        row.add("Sony");
+        row.add("10000");
+        data.add(row);
+        row=new Vector<String>();
+        row.add("202");
+        row.add("Deepthi");
+        row.add("50000");
+        data.add(row);
+        row=new Vector<String>();
+        row.add("204");
+        row.add("Sruthi");
+        row.add("40000");
+        data.add(row);
+        Vector<String> clmn=new Vector<String>();
+        clmn.add("emp id");
+        clmn.add("name");
+        clmn.add("salary");
+        tab=new JTable(data,clmn);
+        JTableHeader h=tab.getTableHeader();
+        Container c=getContentPane();
+        c.setLayout(new BorderLayout());
+        tab.setRowHeight(35);
+        tab.setGridColor(Color.RED);
+        c.add("North",h);
+        c.add("Center",tab);
+    }
+    public static void main(String[] args) {
+        SwingTableTest tt=new SwingTableTest();
+        tt.setTitle("table demo");
+        tt.setSize(400,400);
+        tt.setVisible(true);
+        tt.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
