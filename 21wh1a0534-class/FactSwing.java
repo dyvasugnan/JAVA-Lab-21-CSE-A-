78ed8c4d2e85b3b03b0d6e4b98b23437d7c1657b
21wh1a0534 <137029742+21wh1a0534@users.noreply.github.com>
2023-08-07 21:51:52 +0530

    Add files via upload

---
+++ 21wh1a0534-class/FactSwing.java
@@ -0,0 +1,63 @@
+package java_labcodes;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class FactSwing extends JFrame implements ActionListener {
+    private JButton comp, clear;
+    private JTextField n, res;
+    private JLabel num, result;
+
+    FactSwing() {
+        Container contentPane = getContentPane();
+        contentPane.setLayout(null);
+        comp = new JButton("comp");
+        clear = new JButton("Clear");
+        num = new JLabel("Number");
+        result = new JLabel("Result");
+        n = new JTextField();
+        res = new JTextField();
+        num.setBounds(100, 150, 100, 50);
+		n.setBounds(200, 150, 100, 50);
+		result.setBounds(320, 150, 100, 50);
+		res.setBounds(450, 150, 100, 50);
+		comp.setBounds(150, 350, 100, 50);
+		clear.setBounds(300,350,100,50);
+        contentPane.add(comp);
+        contentPane.add(clear);
+        contentPane.add(num);
+        contentPane.add(result);
+        contentPane.add(n);
+        contentPane.add(res);
+        comp.addActionListener(this);
+        clear.addActionListener(this);
+        comp.setActionCommand("comp");
+        clear.setActionCommand("Clear");
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+        String str = ae.getActionCommand();
+        if (str.equals("comp")) {
+            int numValue = Integer.parseInt(n.getText());
+            int c = 1;
+            for (int i = numValue; i > 0; i--) {
+                c = c * i;
+            }
+            res.setText(String.valueOf(c));
+        }
+        if (str.equals("Clear")) {
+            n.setText("");
+            res.setText("");
+        }
+    }
+
+    public static void main(String[] args) {
+        FactSwing t = new FactSwing();
+        t.setTitle("Calculate");
+        t.setSize(450,450);
+        t.setVisible(true);
+        t.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
+
