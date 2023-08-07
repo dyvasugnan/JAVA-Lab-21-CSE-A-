0f9c84ae6a5efe9f2a58b1085ef78f869d457ac3
22wh5a0502 <137030538+22wh5a0502@users.noreply.github.com>
2023-08-07 18:26:50 +0530

    Add files via upload

--- FrameClass.java
+++ FrameClass.java
@@ -1,46 +1,64 @@
 package javalab;
 import java.awt.*;
 import java.awt.event.*;
-
-
 public class FrameClass extends Frame implements ActionListener {
-		Button b1,b2,b3;
-		FrameClass(){
-			this.setLayout(null);
-			b1 = new Button("Green");
-			b2 = new Button("Red");
-			b3 = new Button("Black");
-			b1.setBounds(100, 150, 100, 50);
-			b2.setBounds(100, 250, 100, 50);
-			b3.setBounds(100, 350, 100, 50);
-			this.add(b1);
-			this.add(b2);
-			this.add(b3);
-			b1.addActionListener(this);
-			b2.addActionListener(this);
-			b3.addActionListener(this);
-					}
-		public void actionPerformed(ActionEvent ae) {
-			String str = ae.getActionCommand();
-			if(str == "Green")
-					this.setBackground(Color.green);
-			if(str == "Red")
-				this.setBackground(Color.red);
-		
-			if(str == "Black")
-				this.setBackground(Color.black);
-		
-		}
-	
-	public static void main(String[] args) {
-		FrameClass f = new FrameClass();
-		f.setTitle("Test");
-		f.setSize(400, 400);
-		f.setVisible(true);
-		f.addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent we) {
-				System.exit(0);
-			}
-		});
+	Button comp,clear;
+	TextField n,res;
+	Label num,result;
+	FrameClass(){
+		this.setLayout(null);
+		comp = new Button("comp");
+		clear=new Button("Clear");
+		num = new Label("Number");
+		result = new Label("fact");
+		n = new TextField();
+		res = new TextField();
+		num.setBounds(100, 150, 100, 50);
+		n.setBounds(100, 150, 100, 50);
+		result.setBounds(100, 150, 100, 50);
+		res.setBounds(100, 150, 100, 50);
+		comp.setBounds(150, 350, 100, 50);
+		clear.setBounds(300,350,100,50);
+		this.add(comp);
+		this.add(clear);
+		this.add(num);
+		this.add(result);
+		this.add(n);
+		this.add(res);
+		comp.addActionListener(this);
+		clear.addActionListener(this);
+		n.addActionListener(this);
+		res.addActionListener(this);
 	}
+	public void actionPerformed(ActionEvent ae)
+	{
+		String str = ae.getActionCommand();
+		if(str == "comp")
+		{
+			int num = Integer.parseInt(n.getText());
+			int c = 1;
+			for(int i=num;i>0;i--) {
+				c=c*i;
+			}
+			res.setText(String.valueOf(c));
+		}
+		if(str == "Clear")
+		{
+			n.setText("");
+			res.setText("");
+			
+		}
+	}
+	public static void main(String[] args) {
+	FrameClass t = new FrameClass();
+	t.setTitle("Calculate");
+	t.setSize(450,450);
+	t.setVisible(true);
+	t.addWindowListener(new WindowAdapter() {
+		public void windowClosing(WindowEvent we) {
+			System.exit(0);
+		}
+	});
 }
+}
+
