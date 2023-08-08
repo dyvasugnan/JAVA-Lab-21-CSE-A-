f4b249b33ef5e19f80a647d782c26549a6829c31
Macherla gormi arshitha <136320117+21wh1a0565@users.noreply.github.com>
2023-08-08 07:15:17 +0530

    Add files via upload

--- 21wh1a0565/FrameClass.java
+++ 21wh1a0565/FrameClass.java
@@ -3,59 +3,58 @@ import java.awt.event.*;
 
 public class FrameClass extends Frame implements ActionListener {
     Button b1, clearBtn;
-    TextField n1, n2, res;
-    Label num1, num2, result;
-    
+    TextField n1, n2;
+    Label num1, res;
+
     FrameClass() {
         this.setLayout(null);
-        
-        b1 = new Button("Add");
+
+        b1 = new Button("Compute");
         clearBtn = new Button("Clear");
-        
+
         num1 = new Label("Number 1");
-        num2 = new Label("Number 2");
-        result = new Label("Result");
-        
+        res = new Label("RESULT");
+
         n1 = new TextField();
         n2 = new TextField();
-        res = new TextField();
-        
+
         num1.setBounds(100, 150, 100, 50);
         n1.setBounds(200, 150, 100, 50);
-        num2.setBounds(100, 250, 100, 50);
+        res.setBounds(100, 250, 100, 50);
         n2.setBounds(200, 250, 100, 50);
-        result.setBounds(100, 350, 100, 50);
-        res.setBounds(200, 350, 100, 50);
+
         b1.setBounds(150, 450, 100, 50);
         clearBtn.setBounds(150, 500, 100, 50);
-        
+
         this.add(b1);
         this.add(clearBtn);
         this.add(num1);
-        this.add(num2);
-        this.add(result);
+        this.add(res);
         this.add(n1);
         this.add(n2);
-        this.add(res);
-        
+
         b1.addActionListener(this);
-        clearBtn.addActionListener(this);
+
+        clearBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                n1.setText("");
+                n2.setText("");
+            }
+        });
     }
-    
+
     public void actionPerformed(ActionEvent ae) {
         String str = ae.getActionCommand();
-        if (str.equals("Add")) {
-            int a = Integer.parseInt(n1.getText());
-            int b = Integer.parseInt(n2.getText());
-            int c = a + b;
-            res.setText(String.valueOf(c));
-        } else if (str.equals("Clear")) {
-            n1.setText("");
-            n2.setText("");
-            res.setText("");
+        if (str.equals("Compute")) {
+            int n = Integer.parseInt(n1.getText());
+            int fact = 1;
+            for (int i = 1; i <= n; i++) {
+                fact *= i;
+            }
+            n2.setText(Integer.toString(fact)); // Display the result in the n2 TextField
         }
     }
-    
+
     public static void main(String[] args) {
         FrameClass f = new FrameClass();
         f.setTitle("Test");
@@ -68,4 +67,5 @@ public class FrameClass extends Frame implements ActionListener {
         });
     }
 }
+
 
