787d30d1300c16df7e574abdb93094867c5c3f3d
Ananya <124264710+21wh1a0531@users.noreply.github.com>
2023-08-08 21:38:32 +0530

    Create Input.java

---
+++ 21wh1a0531-class/Input.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Input {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+        //String s = br.readLine();
+        System.out.println(x);
+        //System.out.println(s);
+    }
+}
