d52ad69f3ed3c68817bf8f4bd5c10b0a840d65ab
21wh1a0517 <137029592+21wh1a0517@users.noreply.github.com>
2023-08-09 23:46:17 +0530

    Add files via upload

---
+++ 21wh1a0517-Class/String_Palindrome.java
@@ -0,0 +1,22 @@
+package javalab;
+
+import java.util.Scanner;
+
+public class String_Palindrome {
+	public static void main(String args[]){
+		String str, rev = "";
+		Scanner sc = new Scanner(System.in);
+ 
+		System.out.print("Enter a string:");
+		str = sc.nextLine();
+		int length = str.length();
+
+		for ( int i = length - 1; i >= 0; i-- )
+			rev = rev + str.charAt(i);
+		if (str.equals(rev))
+			System.out.println(str+" is a palindrome");
+		else
+			System.out.println(str+" is not a palindrome");
+	}
+}
+
