2110f2c964d218621e50f5c7e07a1b0d9258042b
Kiranmayee Tallibiona <136068970+kiranmayeet@users.noreply.github.com>
2023-08-11 22:56:04 +0530

    EvenOrOdd.java

---
+++ 21wh1a0559-class/EvenOrOdd.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n%2==0){
+            System.out.println("EVEN");
+        }
+        else{
+            System.out.println("ODD");
+        }
+    }
+}
