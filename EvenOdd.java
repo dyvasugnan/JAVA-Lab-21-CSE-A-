4de459469f508a857058ca583c5a0877b665126a
22wh5a0506 <137284237+22wh5a0506@users.noreply.github.com>
2023-08-11 14:34:43 +0530

    Add files via upload

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,17 +1,14 @@
-import java.util.*;
+package inheritance;
 
-public class Main {
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter a number: ");
-        int num = sc.nextInt();
-
-        if(num % 2 == 0)
-            System.out.println(num + " is even");
-        else
-            System.out.println(num + " is odd");
-    }
+import java.lang.*;
+class EvenOdd{
+	public static void main(String []arg) {
+		int a=3;
+		if(a%2==0) {
+			System.out.print("given num is even");
+		}
+		else {
+			System.out.print("given num is odd");
+		}
+	}
 }
