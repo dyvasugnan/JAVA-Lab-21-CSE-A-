e647ad3fa09d3b3f658ae1836594e395c1630c9e
21wh1a0534 <137029742+21wh1a0534@users.noreply.github.com>
2023-08-13 19:40:20 +0530

    Add files via upload

---
+++ 21wh1a0534-class/ClassTest.java
@@ -0,0 +1,72 @@
+package java_labcodes;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class ClassTest extends JFrame implements ActionListener {
+	JButton l,r,u,d;
+	int x,y;
+	ClassTest() {
+		Container c = getContentPane();
+		 x=260;
+		 y=120;
+		c.setLayout(null);
+		l = new JButton("left");
+		r = new JButton("right");
+		u = new JButton("up");
+		d = new JButton("down");
+		l.setBounds(180,250,100,50);
+		r.setBounds(290,250,100,50);
+		u.setBounds(230,190,100,50);
+		d.setBounds(230,310,100,50);
+		c.add(l);
+		c.add(r);
+		c.add(u);
+		c.add(d);
+		l.addActionListener(this);
+        r.addActionListener(this);
+        u.addActionListener(this);
+        d.addActionListener(this);
+		l.setActionCommand("l");
+        r.setActionCommand("r");
+        u.setActionCommand("u");
+        d.setActionCommand("d");
+		
+	}
+	public void paint(Graphics g) {
+		super.paint(g);
+		g.setColor(Color.BLUE);
+		//g.drawOval(260,100,50,50);
+		g.fillOval(x,y,50,50);
+    }
+	public void actionPerformed(ActionEvent ae) {
+        String str = ae.getActionCommand();
+        if(str=="l") {
+        	x-=50;
+        	repaint();
+        }
+        if(str=="r") {
+        	x+=50;
+        	repaint();
+        }
+        if(str=="u") {
+        	y-=50;
+        	repaint();
+        }
+        if(str=="d") {
+        	y+=50;
+        	repaint();
+        }
+        
+    }
+	public static void main(String[] args) {
+		ClassTest f = new ClassTest();
+		f.setTitle("Test");
+		f.setSize(600, 600);
+		f.setVisible(true);
+		f.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent we) {
+				System.exit(0);
+			}
+		});
+	}
+}
