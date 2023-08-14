31b4d64f85951796c07b9f394a4229b32a8fe0bc
21wh1a0532 <137388003+21wh1a0532@users.noreply.github.com>
2023-08-14 12:07:26 +0530

    Update and rename bubblesort to bubblesort.java

---
+++ 532-lab/bubblesort.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+class Bubblesort{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n,i;
+        n = sc.nextInt();
+        int a[] = new int[n];
+        int count=0;
+        for(i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        for(i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(a[j]<a[j+1]){
+                    int temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                    count++;
+                }
+            }
+        }
+        for(i=0;i<n;i++)
+            System.out.print(a[i]+" ");
+        System.out.println("");
+        System.out.println(count);
+        
+    }
+}
