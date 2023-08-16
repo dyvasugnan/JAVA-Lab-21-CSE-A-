264a79036394613adc498e0d355d9603542b8be4
21WH1A0506 <137029990+21WH1A0506@users.noreply.github.com>
2023-08-16 09:08:41 +0530

    Create Bubblesort.java

---
+++ 21wh1a0506-Lab/Bubblesort.java
@@ -0,0 +1,27 @@
+import java.lang.*;
+import java.util.*;
+class BubbleSort {
+    public static void main(String[] args) {
+        int n,count=0;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int a[] = new int[n];
+        for(int i=0;i<n;i++){
+            a[i] = sc.nextInt();
+        }
+        for( int i=0;i<n-1;i++){
+            for( int j=0;j<n-i-1;j++){
+                if(a[j]<a[j+1]){
+                    int temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                    count++;
+                }
+            }
+        }
+        for( int i=0;i<n;i++){
+            System.out.println(a[i]);
+        }
+        System.out.println(count);
+    }
+}
