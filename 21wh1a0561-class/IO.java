b1facf277f3e117ce9f75299749a5cb25e20f1d8
Poojitha-61 <136248610+Poojitha-61@users.noreply.github.com>
2023-08-16 22:55:07 +0530

    Add files via upload

---
+++ 21wh1a0561-class/IO.java
@@ -0,0 +1,12 @@
+package javalab;
+import java.lang.*;
+import java.io.*;
+public class IO {
+	public static void main(String args[])throws IOException{
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		System.out.print("Enter string : ");
+		String s = br.readLine();
+		System.out.print(s);
+	}
+}
