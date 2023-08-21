aa8da422137e5d9f3dc0aceb2cf96dcfbe86150f
21wh1a0542 <137029747+21wh1a0542@users.noreply.github.com>
2023-08-21 08:24:24 +0530

    Create OddOrEven.java

---
+++ 21wh1a0542 - Class/OddOrEven.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class OddOrEven{
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        long n = scaner.nextLong();
+        if(n%2==0)
+        System.out.println("Even Number");
+        else
+        System.out.println("Odd Number");
+    }
+}
