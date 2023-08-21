30a8f01ae13e1df027ab41dce3a52c0fd8ae22c9
harshitha <124253997+21wh1a0509@users.noreply.github.com>
2023-08-21 23:16:47 +0530

    Add files via upload

---
+++ 21wh1a0509_LAB/QuickSortLast.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class QuickSortLast{
+    public static void quickSort(int[] a, int low, int high) {
+        if (low < high) {
+            int j = partition(a, low, high);
+            quickSort(a, low, j - 1);
+            quickSort(a, j + 1, high);
+        }
+    }    
+
+    public static int partition(int a[], int low, int high) {
+        int p = a[high];
+        int i = low;
+        int j = high-1;
+        
+        while (i <= j) {
+            while (i <= j && a[i] <= p) 
+                i++;
+            while (i <= j && a[j] >= p) 
+                j--;
+            if (i < j){ 
+            	int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+        int t = a[high];
+        a[high] = a[i];
+        a[i] = t;
+        return i;
+    }
+    
+    @SuppressWarnings("resource")
+	public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++)
+            a[i] = sc.nextInt();
+        quickSort(a, 0, n - 1);
+        for (int i = 0; i < n; i++) 
+            System.out.print(a[i] + " ");
+    }    
+}
