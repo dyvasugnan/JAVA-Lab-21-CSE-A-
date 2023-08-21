72462af8bc1dd27c13b5d1c57c48753287a6b845
21wh1a0508 <136461092+21wh1a0508udayasri@users.noreply.github.com>
2023-08-21 18:41:47 +0530

    QuickSortFristAsPivot.java

---
+++ 21wh1a0508_lab/QuickSortFristAsPivot.java
@@ -0,0 +1,50 @@
+package javaClass;
+import java.util.*;
+public class QuickSortFirstAsPivot {
+    public static int partition(int arr[], int low, int high) {
+        int pivot = arr[low];
+        int i = low+1;
+        int j = high;
+
+        while (i <= j) {
+            while (i <= j && arr[i] <= pivot) {
+                i++;
+            }
+            while (i <= j && arr[j] > pivot) {
+                j--;
+            }
+            if (i < j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[low];
+        arr[low] = arr[j];
+        arr[j] = temp;
+
+        return j;
+    }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        quickSort(a, 0, n - 1);
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+}
