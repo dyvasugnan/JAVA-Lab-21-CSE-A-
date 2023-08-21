aa6a0172d211ebc6da55c00ecaac0e2d0b21d14d
21wh1a0542 <137029747+21wh1a0542@users.noreply.github.com>
2023-08-21 08:22:06 +0530

    Add files via upload

---
+++ 21wh1a0542 - Class/BubbleSort.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class BubbleSort{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter the elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        bubbleSort(arr);
+
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        sc.close();
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                  
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+}
