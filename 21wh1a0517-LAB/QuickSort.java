23fe75470eb516320687f69aef879e983a60bf34
21wh1a0517 <137029592+21wh1a0517@users.noreply.github.com>
2023-08-21 21:42:03 +0530

    Add files via upload

---
+++ 21wh1a0517-LAB/QuickSort.java
@@ -0,0 +1,57 @@
+package javalab;
+import java.util.*;
+public class QuickSort {
+	public static void quickSort(int[] a, int low, int high){
+        if (low < high){
+            int j = partition(a, low, high);
+            quickSort(a, low, j - 1);
+            quickSort(a, j + 1, high);
+        }
+    }    
+
+    public static int partition(int a[], int low, int high){
+        int p = a[low];
+        int i = low + 1;
+        int j = high;
+
+        while (i <= j)
+        {
+            while (i <= j && a[i] <= p)
+            {
+                i++;
+            }
+            while (i <= j && a[j] >= p) 
+            {
+                j--;
+            }
+            if (i < j)
+            {
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+        int temp = a[low];
+        a[low] = a[j];
+        a[j] = temp;
+
+        return j;
+    }
+
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = sc.nextInt();
+        }
+        quickSort(a, 0, n - 1);
+        for (int i = 0; i < n; i++)
+        {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }    
+}
