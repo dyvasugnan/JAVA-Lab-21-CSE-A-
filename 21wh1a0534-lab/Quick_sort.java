e3c92a3abe7513de9f139ba7601144857c1de3aa
21wh1a0534 <137029742+21wh1a0534@users.noreply.github.com>
2023-08-21 22:49:02 +0530

    Add files via upload

---
+++ 21wh1a0534-lab/Quick_sort.java
@@ -0,0 +1,57 @@
+package java_labcodes;
+import java.util.Scanner;
+
+public class Quick_sort {
+
+    static void swap(int a[], int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    static int partition(int a[], int low, int high) {
+        int i = low;
+        int j = high - 1;
+        int p = a[high];
+
+        while (i <= j) {
+            while (a[i] <= p) {
+                i++;
+            }
+            while (a[j] > p) {
+                j--;
+            }
+            if (i < j) {
+                swap(a, i, j);
+            }
+        }
+        swap(a, i, high);
+        return i;
+    }
+
+    static void quicksort(int a[], int low, int high) {
+        if (low < high) {
+            int j = partition(a, low, high);
+            quicksort(a, low, j - 1);
+            quicksort(a, j + 1, high);
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int n = sc.nextInt();
+        System.out.println("Enter array elements:");
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        quicksort(a, 0, n - 1);
+        System.out.println("Elements after sorting:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+}
+
+	
