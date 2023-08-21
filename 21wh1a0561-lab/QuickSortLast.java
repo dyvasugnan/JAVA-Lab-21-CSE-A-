a059485aefe6fd2eca07a62d7ecfaf48a09e7f01
Poojitha-61 <136248610+Poojitha-61@users.noreply.github.com>
2023-08-21 22:08:37 +0530

    Add files via upload

---
+++ 21wh1a0561-lab/QuickSortLast.java
@@ -0,0 +1,46 @@
+package javalab;
+import java.util.*;
+public class QuickSortLast {
+
+	static int partition(int arr[], int low, int high){
+        int pivot = arr[high];
+        int i = low-1;
+        for(int j = low ; j < high; j++){
+            if(arr[j] < pivot){
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        i++;
+        int temp = arr[i];
+        arr[i] = pivot;
+        arr[high] = temp;
+        
+        return i;
+    }
+    static void quicksort(int arr[], int low, int high){
+    	if(low < high) {
+        int pivId = partition(arr,low,high);
+        quicksort(arr,low,pivId-1);
+        quicksort(arr,pivId+1,high);
+    	}
+    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the size of the array : ");
+		int n = sc.nextInt();
+		int a[] = new int[n];
+		for(int i = 0; i <n; i++){
+		    a[i] = sc.nextInt();
+		}
+		quicksort(a,0,n-1);
+		System.out.println("The sorted array after quicksort is : ");
+		for(int i = 0; i < n; i++){
+		    System.out.print(a[i]+ " ");
+		}
+		System.out.println();
+	}
+
+}
