f307490cb05ed26f97bd5150d9ab8abc7366ceda
21wh1a0508 <136461092+21wh1a0508udayasri@users.noreply.github.com>
2023-08-26 21:40:26 +0530

    Add files via upload

---
+++ 21wh1a0508-class/HashTableFile.java
@@ -0,0 +1,30 @@
+package javaClass;
+import java.io.*;
+import java.util.*;
+
+public class HashTableFile 
+{
+    public static void main(String args[]) throws FileNotFoundException
+    {
+        FileInputStream fis = new FileInputStream("C:\\Users\\DELL\\Desktop\\HashTable.txt");
+        Scanner sc = new Scanner(fis).useDelimiter(",");
+        Hashtable<String, String> ht = new Hashtable<String, String>();
+        String[] str;
+        String s;
+        while (sc.hasNextLine()) 
+        {
+            s = sc.nextLine();
+            str = s.split(",");
+            ht.put(str[0], str[1]);
+        }
+        
+        Enumeration<String> keys = ht.keys();  
+        while (keys.hasMoreElements())
+        {
+            String key = keys.nextElement();
+            String value = ht.get(key);
+            System.out.println("Key: " + key + ", Value: " + value);
+        }
+        sc.close();
+    }    
+}
