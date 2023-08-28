f70411ca181bc7f823fb961bdce51bdd4294e66f
21wh1a0523 <137030067+21wh1a0523@users.noreply.github.com>
2023-08-28 08:12:24 +0530

    Add files via upload

---
+++ 21wh1a0523/SubArray.java
@@ -0,0 +1,39 @@
+package labprograms;
+import java.util.*;
+	public class SubArray 
+	{
+	    public static void subArray(ArrayList<Integer> v, int start, int end)
+	    {
+	        if (start == v.size()) 
+	        {
+	            return;
+	        } 
+	        else if (start > end)
+	        {
+	            subArray(v, 0, end + 1);
+	        }
+	        else
+	        {
+	            System.out.print("[");
+	            for (int i = start; i < end; i++)
+	            {
+	                System.out.print(v.get(i) + " ");
+	            }
+	            System.out.println(v.get(end) + "]");
+	            subArray(v, start + 1, end);
+	        }
+	        return;
+	    }
+
+	    public static void main(String[] args) 
+	    {
+	        Scanner sc = new Scanner(System.in);
+	        int n = sc.nextInt();
+	        ArrayList<Integer> a = new ArrayList<>();
+	        for (int i = 0; i < n; i++)
+	        {
+	            a.add(sc.nextInt());
+	        }
+	        subArray(a, 0, 0);
+	    }
+	}
