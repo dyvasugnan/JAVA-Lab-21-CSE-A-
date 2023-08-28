916a074cd7e928a8628c7c849321874de6a2616c
21wh1a0556 <137030078+21wh1a0556@users.noreply.github.com>
2023-08-28 21:08:34 +0530

    Create DoublyLinkedList.java

---
+++ 21wh1a0556-lab/DoublyLinkedList.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class DoublyLinkedList {
+	public static void main(String args[]) {
+	Scanner sc = new Scanner(System.in);
+	LinkedList<Integer>Lis = new LinkedList<Integer>();
+	System.out.print("1.add 2.delete 3.display\n");
+	
+	while(true) {
+		int ch = sc.nextInt();
+		switch(ch) {
+		case 1:System.out.print("enter the element to be pushed ");
+		int ele = sc.nextInt();
+		Lis.add(ele);
+		break;
+		
+		case 2:int e = Lis.removeFirst();
+		System.out.print(e);
+		break;
+		
+		case 3:Iterator it = Lis.iterator();
+		while(it.hasNext()) {
+			System.out.print(it.next() + " ");
+		}
+		break;
+		 
+		case 4 : System.exit(0);
+		
+		default : System.out.print("element not found\n");
+		}
+	}
+}
+}
