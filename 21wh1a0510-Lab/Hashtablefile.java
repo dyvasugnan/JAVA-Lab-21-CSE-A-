37a70b0a9ca455eafbb69898372163b3be2d4c88
Alapati Nithika <121952199+AlapatiNithika@users.noreply.github.com>
2023-08-29 20:51:28 +0530

    Update Hashtablefile.java

--- 21wh1a0510-Lab/Hashtablefile.java
+++ 21wh1a0510-Lab/Hashtablefile.java
@@ -2,39 +2,51 @@ package Javaclss;
 import java.util.*;
 import java.io.*;
 
-public class Hashtablefile {
-    public static void main(String[] args) throws FileNotFoundException {
+class EmployeeData {
+    private Hashtable<String, String> employeeSalaryData = new Hashtable<>();
+
+    public EmployeeData(String filePath) {
         try {
-            FileInputStream fis = new FileInputStream("C:\\Users\\91707\\eclipse-workspace\\JavaLab\\src\\Javaclss\\student.txt");
+            FileInputStream fis = new FileInputStream(filePath);
             Scanner sc = new Scanner(fis);
-            Hashtable<String, String> ht = new Hashtable<>();
-            String[] str;
-            String s;
+            
             while (sc.hasNextLine()) {
-                s = sc.nextLine();
-                str = s.split(",");
-                if (str.length == 2) { 
-                    ht.put(str[0], str[1]);
+                String s = sc.nextLine();
+                String[] str = s.split(",");
+                if (str.length == 3) {
+                    employeeSalaryData.put(str[0], str[1]);
                 }
             }
-            Enumeration<String> e = ht.keys(); 
-            while (e.hasMoreElements()) {
-            	String key = e.nextElement();
-                String value = ht.get(key);
-                System.out.println("Student: " + key + ", marks: " + value);
-            }
-            System.out.println("Provide name: ");
-            Scanner scanner = new Scanner(System.in);
-            String s1 = scanner.next();
-            if(ht.containsKey(s1)) {
-            	String s2 = ht.get(s1);
-            	System.out.println("marks: "+s2);
-            }
-            else {
-            	System.out.println("Not found!!!");
-            }
+            
+            sc.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
+    public Hashtable<String, String> getEmployeeSalaryData() {
+        return employeeSalaryData;
+    }
 }
+public class Hashtablefile {
+    public static void main(String[] args) {
+        EmployeeData employeeData = new EmployeeData("C:\\Users\\91707\\eclipse-workspace\\JavaLab\\src\\Javaclss\\employee.txt");
+        Hashtable<String, String> ht = employeeData.getEmployeeSalaryData();
+        Enumeration<String> e = ht.keys(); 
+        while (e.hasMoreElements()) {
+            String key = e.nextElement();
+            String value = ht.get(key);
+            System.out.println("Employee: " + key + ", Salary: " + value);
+        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Employee Number : ");
+        String s1 = scanner.next();
+        if (ht.containsKey(s1)) {
+            String s2 = ht.get(s1);
+            System.out.println("Salary: " + s2);
+        } else {
+            System.out.println("Details not found!!!");
+        }
+    }
+}
+
+
