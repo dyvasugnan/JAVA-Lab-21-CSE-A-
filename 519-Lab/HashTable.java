f84496153325b29c9669d4f8108665eb040b1dd8
21wh1a0519 <137029965+21wh1a0519@users.noreply.github.com>
2023-08-29 21:40:06 +0530

    Add files via upload

---
+++ 519-Lab/HashTable.java
@@ -0,0 +1,49 @@
+package java_labcodes;
+import java.lang.*;
+import java.util.*;
+public class HashTable {
+       public static void main(String args[]) {
+    	   Scanner sc = new Scanner(System.in);
+    	   Hashtable<Integer,Integer> ht = new Hashtable<Integer,Integer>();
+    	   while(true) {
+    		   System.out.println("1.Put\t2.Get\t3.Remove\t4.Display\t5.size\t6.check\t7.Exit");
+    		   System.out.println("Enter your choice:");
+    		   int ch = sc.nextInt();
+    		   switch(ch) {
+    		   case 1:
+    			   System.out.println("Enter key and value: ");
+    			   int k = sc.nextInt();
+    			   int v = sc.nextInt();
+    			   ht.put(k, v);
+    			   break;
+    		   case 2:
+    			   System.out.println("Value of key: ");
+    			   int key = sc.nextInt();
+    			   System.out.println(ht.get(key));
+    			   break;
+    		   case 3:
+    			   System.out.println("Remove key: ");
+    			   int keyr = sc.nextInt();
+    			   ht.remove(keyr);
+    			   break;
+    		   case 4:
+    			   System.out.println(ht);
+    			   break;
+    		   case 5 :
+    			   System.out.println("size of the table is "+ht.size());
+    			   break;
+    		   case 6 :
+    			   System.out.print("enter the key to check");
+    			   int ky = sc.nextInt();
+    			   boolean pres = ht.containsKey(ky);
+    			   System.out.println(pres);
+    			   break;
+    		   case 7:
+    			   System.exit(0);
+    		   }
+
+    	   }
+
+       }
+
+}
