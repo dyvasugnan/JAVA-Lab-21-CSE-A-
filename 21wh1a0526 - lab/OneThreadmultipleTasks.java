3d636e2098fb5e59cdedc670f98913d9a707ae54
21wh1a0526 <137029959+21wh1a0526@users.noreply.github.com>
2023-09-04 11:15:12 +0530

    Create OneThreadmultipleTasks.java

---
+++ 21wh1a0526 - lab/OneThreadmultipleTasks.java
@@ -0,0 +1,35 @@
+package cse526;
+
+public class OneThreadmultipleTasks 
+{
+
+	public static void main(String[] args) 
+	{
+		MyThread obj =  new MyThread();
+		Thread t1 = new Thread(obj);
+		t1.start();
+	}
+}
+
+class MyThread extends Thread
+{
+	public void run()
+	{
+		task1();
+		task2();
+		task3();
+	}
+		public void task1()
+		{
+			System.out.println("task 1 is executed");
+		}
+		public void task2()
+		{
+			System.out.println("task 2 is executed");
+		}
+		public void task3()
+		{
+			System.out.println("task 3 is executed");
+		}
+	}
+
