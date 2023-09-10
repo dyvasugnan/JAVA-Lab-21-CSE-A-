8173f6a97348515aeccbb05c6f382f2dcafd36ac
21wh1a0519 <137029965+21wh1a0519@users.noreply.github.com>
2023-09-10 11:59:18 +0530

    Add files via upload

---
+++ 519-Lab/BubbleSortDes.java
@@ -0,0 +1,28 @@
+package java_codes;
+
+import java.util.*;
+public class BubbleSortDes {
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter the n value : ");
+	        int n = sc.nextInt();
+	        int a[] = new int[n];
+	        for(int i = 0;i<n;i++)
+	        a[i] = sc.nextInt();
+	        int count = 0;
+	        for(int i = 0;i<n;i++){
+	            for(int j = 0;j<n-i-1;j++){
+	                if(a[j]<a[j+1]){
+	                    int t = a[j];
+	                    a[j] = a[j+1];
+	                    a[j+1] = t;
+	                    count++;
+	                }
+	            }
+	        }
+	        System.out.print("Sorted array in desending order : ");
+	        for(int i = 0;i<n;i++)
+	        System.out.println(a[i]+" ");
+	        System.out.print("The number of swaps are :"+count);
+	    }
+	}
