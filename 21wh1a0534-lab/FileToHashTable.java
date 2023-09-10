faed16119323ec80f33e1d6df3e542cf7705e5bf
21wh1a0534 <137029742+21wh1a0534@users.noreply.github.com>
2023-09-10 21:21:02 +0530

    Add files via upload

---
+++ 21wh1a0534-lab/FileToHashTable.java
@@ -0,0 +1,38 @@
+package java_labcodes;
+import java.io.*;
+import java.util.*;
+public class FileToHashTable {
+    public static void main(String args[]) {
+        try {
+            FileInputStream fis = new FileInputStream("C:\\Users\\teju1\\OneDrive\\Desktop\\java");
+            Scanner sc = new Scanner(fis).useDelimiter("\t");
+
+            Hashtable<String, String> ht = new Hashtable<String, String>();
+
+            System.out.println("hash table values are : ");
+            while (sc.hasNextLine()) {
+                String s = sc.nextLine();
+                String[] str = s.split("\t");
+                if (str.length == 2) {
+                    ht.put(str[0], str[1]);
+                    System.out.println(str[0] + " : " + str[1]);
+                }
+            }
+
+            Scanner sca = new Scanner(System.in);
+            System.out.print("Enter the name as given in the phone book: ");
+            String name = sca.next();
+
+            String phoneNumber = ht.get(name);
+            if (phoneNumber != null) {
+                System.out.println("Phone number is: " + phoneNumber);
+            } else {
+                System.out.println("Name not found in the phone book.");
+            }
+
+            fis.close(); // Close the FileInputStream
+        } catch (IOException e) {
+            System.err.println("An error occurred: " + e.getMessage());
+        }
+    }
+}
